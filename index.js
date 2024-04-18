c6e7d26ccdb035cbb3a6e4ef8fe31e00cf0e30a3
ViacheslavMiakota <miakotaviacheslav1@gmail.com>
2024-04-19 00:48:39 +0300

    added another option

--- index.js
+++ index.js
@@ -1,10 +1,27 @@
-const fs = require('fs');
+// const fs = require('fs');
 
-const sumNumbers = (fileA, fileB) => {
-    const numberA = parseInt(fs.readFileSync(fileA, 'utf8'));
-    const numberB = parseInt(fs.readFileSync(fileB, 'utf8'));
-    const sum = numberA + numberB;
-    console.log(sum);
+// const sumNumbers = (fileA, fileB) => {
+//     const numberA = parseInt(fs.readFileSync(fileA, 'utf8'));
+//     const numberB = parseInt(fs.readFileSync(fileB, 'utf8'));
+//     const sum = numberA + numberB;
+//     console.log(sum);
+// }
+
+const fs = require('fs/promises');
+
+const sumNumbers = async (fileA, fileB) => {
+    try {
+        const dataA = await fs.readFile(fileA, 'utf8');
+        const dataB = await fs.readFile(fileB, 'utf8');
+        
+        const numberA = parseInt(dataA);
+        const numberB = parseInt(dataB);
+        
+        const sum = numberA + numberB;
+        console.log(sum);
+    } catch (error) {
+        console.error('Error:', error);
+    }
 }
 
 sumNumbers('a.txt', 'b.txt');
