a15896630ee72eb314411a7cdabe9e2ee586b713
ViacheslavMiakota <miakotaviacheslav1@gmail.com>
2024-05-03 18:56:02 +0300

    corect server db

--- src/config/db.js
+++ src/config/db.js
@@ -11,9 +11,9 @@ const connectDb = async () => {
     const client = new MongoClient(MONGO_DB_CONNECTION);
 
     const connect = await client.connect();
-    const db = connect.db();
+    const db = connect.db(DB_NAME);
     const users = db.collection("users");
-    console.log(users);
+    // console.log(users);
     console.log(`Database connection successful`.white.bold.bgGreen);
   } catch (error) {
     console.log("Can not conect to the  DB: ".white.bgRed.bold, error);
